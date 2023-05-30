c5e214db330e69a41e42f14a3f8fae79a0affe72
MaevaWolff <maeva.wolff@hetic.net>
2023-05-30 19:17:37 +0200

    feat: add red color for heart and diamond suits

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,20 @@
 import type { Card } from "@/entities/types";
 import { MAP_SUIT_TO_SYMBOL } from "@/entities/utils";
+import clsx from "clsx";
 import React from "react";
 
 export default function Card({ suit, value }: Card) {
+  const isRedCard = suit === "hearts" || suit === "diamonds";
+
   return (
-    <div className="flex h-28 w-20 flex-col justify-between rounded-md bg-white p-2">
+    <div
+      className={clsx(
+        "flex h-28 w-20 flex-col justify-between rounded-md bg-white p-2",
+        {
+          ["text-red-500"]: isRedCard,
+        }
+      )}
+    >
       <p className="font-bold">{value}</p>
       <p className="text-2xl">{MAP_SUIT_TO_SYMBOL[suit]}</p>
     </div>
