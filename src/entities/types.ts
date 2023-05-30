1a613dfa91129f5fc4bec0b7e1963691382ba095
MaevaWolff <maeva.wolff@hetic.net>
2023-05-30 06:09:08 +0200

    feat: create card entity with deck of cards

---
+++ src/entities/types.ts
@@ -0,0 +1,7 @@
+export type Suit = "hearts" | "diamonds" | "clubs" | "spades";
+export type Value = "A" | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | "J" | "K" | "Q";
+
+export interface Card {
+  suit: Suit;
+  value: Value;
+}
---
+++ src/entities/utils.ts
@@ -0,0 +1,10 @@
+import type { Suit } from "./types";
+
+const MAP_SUIT_TO_SYMBOL: Record<Suit, string> = {
+  hearts: "♥",
+  diamonds: "♦",
+  clubs: "♣",
+  spades: "♠",
+};
+
+export { MAP_SUIT_TO_SYMBOL };
