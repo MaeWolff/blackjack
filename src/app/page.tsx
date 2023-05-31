ac98e7a64333fdc6841fe04ceb3c398474d58d07
MaevaWolff <maeva.wolff@hetic.net>
2023-06-01 00:07:32 +0200

    feat: add logic of blackjack game (first iteration)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,186 @@
+"use client";
+import Card, { InitialCard } from "@/components/Card";
+import deck from "@/entities/deck-of-cards";
+import type { Card as CardType } from "@/entities/types";
+import {
+  MAP_VALUE_TO_SCORE,
+  calculateWinner,
+  drawRandomCardIndex,
+  updateDeck,
+} from "@/entities/utils";
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  // GAME
+  const [currentDeck, setCurrentDeck] = useState(deck);
+  const [gameState, setGameState] = useState<
+    "initial" | "started" | "dealerTurn" | "finished"
+  >("initial");
+
+  // DEALER
+  const [dealerDeck, setDealerDeck] = useState<[] | CardType[]>([]);
+  const [dealerScore, setDealerScore] = useState<number>(0);
+
+  // PLAYER
+  const [myDeck, setMyDeck] = useState<[] | CardType[]>([]);
+  const [myScore, setMyScore] = useState<number>(0);
+
+  function handlePlayGame() {
+    const dealerCardIndex = drawRandomCardIndex();
+    const myCardIndex1 = drawRandomCardIndex();
+    const myCardIndex2 = drawRandomCardIndex();
+
+    setDealerDeck((oldCards) => [...oldCards, currentDeck[dealerCardIndex]]);
+    setMyDeck((oldCards) => [
+      ...oldCards,
+      currentDeck[myCardIndex1],
+      currentDeck[myCardIndex2],
+    ]);
+
+    const newDeck = updateDeck(currentDeck, [
+      dealerCardIndex,
+      myCardIndex1,
+      myCardIndex2,
+    ]);
+    setCurrentDeck(newDeck);
+
+    if (myScore >= 21) setGameState("finished");
+    setGameState("started");
+  }
+
+  function handleHit() {
+    if (myScore >= 21) setGameState("finished");
+    const randomCardIndex = drawRandomCardIndex();
+
+    setMyDeck((oldCards) => [...oldCards, currentDeck[randomCardIndex]]);
+
+    const newDeck = updateDeck(currentDeck, randomCardIndex);
+    setCurrentDeck(newDeck);
+  }
+
+  function handleStand() {
+    setGameState("dealerTurn");
+  }
+
+  function handlePlayAgain() {
+    setDealerDeck([]);
+    setMyDeck([]);
+    setDealerScore(0);
+    setMyScore(0);
+    setGameState("initial");
+    setCurrentDeck(deck);
+  }
+
+  useEffect(() => {
+    let newDealerScore = 0;
+    dealerDeck.forEach((card) => {
+      if (card.value) {
+        newDealerScore += MAP_VALUE_TO_SCORE[card.value];
+      }
+    });
+    setDealerScore(newDealerScore);
+  }, [dealerDeck]);
+
+  useEffect(() => {
+    let newMyScore = 0;
+    myDeck.forEach((card) => {
+      if (card.value) {
+        newMyScore += MAP_VALUE_TO_SCORE[card.value];
+      }
+    });
+    setMyScore(newMyScore);
+  }, [myDeck]);
+
+  useEffect(() => {
+    if (dealerScore < 17 && gameState === "dealerTurn") {
+      const randomCardIndex = drawRandomCardIndex();
+      const newCard = currentDeck[randomCardIndex];
+
+      setDealerDeck((oldCards) => [...oldCards, newCard]);
+
+      const newDeck = updateDeck(currentDeck, randomCardIndex);
+      setCurrentDeck(newDeck);
+
+      setDealerScore(
+        (prevScore) => prevScore + MAP_VALUE_TO_SCORE[newCard.value]
+      );
+    }
+
+    if (dealerScore >= 17) return setGameState("finished");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dealerScore, gameState]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center p-8">
-      <h1>page</h1>
+    <main className="flex w-full flex-col items-center gap-16 p-8">
+      <div className="flex min-h-[200px] w-full max-w-[600px] flex-col items-center justify-between gap-8 md:flex-row md:gap-0">
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-red-500">Dealer ({dealerScore})</p>
+          <div className="flex flex-row gap-2">
+            {gameState === "initial" && <InitialCard />}
+
+            {dealerDeck.map((card, i) => (
+              <Card key={i} {...card} />
+            ))}
+            {dealerDeck.length === 1 && <InitialCard />}
+          </div>
+        </div>
+
+        <div
+          className={clsx(
+            "w-fit rounded-md bg-[#FED3C0] px-4 py-2 font-bold text-red-500",
+            { ["bg-transparent text-transparent"]: gameState !== "finished" }
+          )}
+        >
+          {calculateWinner(dealerScore, myScore)}
+        </div>
+
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-red-500">You ({myScore})</p>
+          <div className="flex flex-row gap-2">
+            {gameState === "initial" && <InitialCard />}
+
+            {myDeck.map((card, i) => (
+              <Card key={i} {...card} />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {gameState === "initial" && (
+        <button
+          className="rounded-md bg-red-500 px-4 py-2 text-white"
+          onClick={handlePlayGame}
+        >
+          Play
+        </button>
+      )}
+
+      {gameState === "started" && (
+        <div className="flex flex-row gap-2">
+          <button
+            className="rounded-md bg-red-500/60 px-4 py-2 text-white"
+            onClick={handleStand}
+          >
+            Stand
+          </button>
+          <button
+            className="rounded-md bg-red-500 px-4 py-2 text-white"
+            onClick={handleHit}
+          >
+            Hit
+          </button>
+        </div>
+      )}
+
+      {gameState === "finished" && (
+        <button
+          className="rounded-md bg-red-500 px-4 py-2 text-white"
+          onClick={handlePlayAgain}
+        >
+          Play again :)
+        </button>
+      )}
     </main>
   );
 }
--- src/entities/utils.ts
+++ src/entities/utils.ts
@@ -1,4 +1,5 @@
-import type { Suit } from "./types";
+import deck from "./deck-of-cards";
+import type { Card, Suit, Value } from "./types";
 
 const MAP_SUIT_TO_SYMBOL: Record<Suit, string> = {
   hearts: "♥",
@@ -7,4 +8,67 @@ const MAP_SUIT_TO_SYMBOL: Record<Suit, string> = {
   spades: "♠",
 };
 
-export { MAP_SUIT_TO_SYMBOL };
+const MAP_VALUE_TO_SCORE: Record<Value, number> = {
+  A: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  10: 10,
+  J: 10,
+  Q: 10,
+  K: 10,
+};
+
+function drawRandomCardIndex(): number {
+  return Math.floor(Math.random() * deck.length);
+}
+
+function removeCardsFromDeck(deck: Card[], cardIndex: number | number[]) {
+  if (typeof cardIndex === "number") return deck.splice(cardIndex, 1);
+  return cardIndex.map((i) => deck.splice(i, 1));
+}
+
+function updateDeck(
+  currentDeck: Card[],
+  cardIndexToDelete: number | number[]
+): Card[] {
+  const newDeck = [...currentDeck];
+  removeCardsFromDeck(newDeck, cardIndexToDelete);
+  return newDeck;
+}
+
+enum ResultMessage {
+  win = "You Win !",
+  lose = "You lose",
+  equality = "Push",
+}
+
+function calculateWinner(dealerScore: number, myScore: number) {
+  if (dealerScore > 21 && myScore <= 21) {
+    return ResultMessage.win;
+  } else if (myScore > 21 && dealerScore <= 21) {
+    return ResultMessage.lose;
+  } else if (myScore === dealerScore) {
+    return ResultMessage.equality;
+  } else if (dealerScore > myScore && dealerScore <= 21) {
+    return ResultMessage.lose;
+  } else if (myScore > dealerScore && myScore <= 21) {
+    return ResultMessage.win;
+  } else {
+    return "Invalid scores";
+  }
+}
+
+export {
+  MAP_SUIT_TO_SYMBOL,
+  MAP_VALUE_TO_SCORE,
+  drawRandomCardIndex,
+  removeCardsFromDeck,
+  updateDeck,
+  calculateWinner,
+};
