09e61635b4d144f0fb991de1406ba59ae0bc36a5
MaevaWolff <maeva.wolff@hetic.net>
2023-06-01 01:15:45 +0200

    feat: add unit test for drawRandomCardIndex function

---
+++ src/entities/utils.test.ts
@@ -0,0 +1,13 @@
+import { describe, it, expect } from "vitest";
+
+import deck from "./deck-of-cards";
+import { drawRandomCardIndex } from "./utils";
+
+describe(drawRandomCardIndex.name, () => {
+  it("returns a valid card index from the deck", () => {
+    const index = drawRandomCardIndex();
+
+    expect(index).toBeGreaterThanOrEqual(0);
+    expect(index).toBeLessThan(deck.length);
+  });
+});
